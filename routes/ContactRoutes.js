942bded672f9da223d4748fa8221064b431774ab
Elx4n13 <elx4n13@gmail.com>
2024-11-01 09:47:45 +0400

    feat:change path file

--- routes/ContactRoutes.js
+++ routes/ContactRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { verifyToken } from "../middlewares/authMiddleware.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
 import {
   getAllContacts,
   getContactsForDMList,
